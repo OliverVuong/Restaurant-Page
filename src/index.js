4e50ce187c321076a1f5ad134967dacdcc894005
Oliver Vuong <vuongoliver@gmail.com>
2023-04-19 01:13:48 -0700

    Add js function that creates all html except for main content

--- src/index.js
+++ src/index.js
@@ -3,6 +3,34 @@ import makeHome from './home';
 import makeMenuPage from './menu';
 import makeContactPage from './contact';
 
+const makePage = () => {
+    const body = document.querySelector('#content');
+
+    const nav = document.createElement('nav');
+
+    const homeBtn = document.createElement('button');
+    const menuBtn = document.createElement('button');
+    const contactBtn = document.createElement('button');
+
+    homeBtn.classList.add('homeBtn');
+    menuBtn.classList.add('menuBtn');
+    contactBtn.classList.add('contactBtn');
+
+    homeBtn.textContent = 'Home';
+    menuBtn.textContent = 'Menu';
+    contactBtn.textContent = 'Contact';
+
+    body.appendChild(nav);
+    nav.appendChild(homeBtn);
+    nav.appendChild(menuBtn);
+    nav.appendChild(contactBtn);
+
+    body.appendChild(document.createElement('main'));
+    body.appendChild(document.createElement('footer'));
+}
+
+makePage();
+
 console.log("hello blah");
 makeHome();
 makeMenuPage();
